735d1f502d9398687c232b68ea56f8a70a4027c7
NeverMind109 <vladgladkih123@gmail.com>
2022-05-04 23:55:02 +0300

    Created: History, Partners, Team in About Us Page

--- src/js/components/sliders.js
+++ src/js/components/sliders.js
@@ -124,7 +124,6 @@ if (relatedSlider) {
 }
 
 const workImages = document.querySelector(".work-images-slider");
-
 if (workImages) {
   const workSliderNav = new Swiper(".work-images-nav", {
     spaceBetween: 20,
@@ -142,5 +141,43 @@ if (workImages) {
     thumbs: {
       swiper: workSliderNav,
     },
+  });
+}
+
+const historyItems = document.querySelector(".history-slider");
+if (historyItems) {
+  const historySlider = new Swiper(historyItems, {
+    spaceBetween: 20,
+    slidesPerView: 1,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: ".history__next",
+      prevEl: ".history__prev",
+    },
+  });
+
+  historySlider.on("slideChange", function () {
+    historyBtns.forEach((el) => {
+      el.classList.remove("history-nav__btn--active");
+    });
+    document
+      .querySelector(
+        `.history-nav__btn[data-index="${historySlider.realIndex}"]`
+      )
+      .classList.add("history-nav__btn--active");
+  });
+
+  const historyBtns = document.querySelectorAll(".history-nav__btn");
+  historyBtns.forEach((el, idx) => {
+    el.setAttribute("data-index", idx);
+    el.addEventListener("click", (e) => {
+      const index = e.currentTarget.dataset.index;
+
+      historyBtns.forEach((el) => {
+        el.classList.remove("history-nav__btn--active");
+      });
+      e.currentTarget.classList.add("history-nav__btn--active");
+      historySlider.slideTo(index);
+    });
   });
 }
